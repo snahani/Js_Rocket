7c47df1f6fdf29b7291d450ef9bef8e0db660fc9
snahani <43251571+snahani@users.noreply.github.com>
2019-08-26 13:59:23 +0430

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+// Types of Variables :
+
+	var favColor = "Blue";
+	var favColorArrey = ['Blue', 'White', 'Rose'];
+	var favColorObject = {
+		name : 'Sobhan',
+		familly : 'AHANI',
+		age : 36;
+		favColors : ['Blue', 'White', 'Rose'],
+		isMale : true,
+	};
+	
