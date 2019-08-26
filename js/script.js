9d45b1723bbab2ad6aa1e178e904d5050d293b14
snahani <43251571+snahani@users.noreply.github.com>
2019-08-27 03:16:12 +0430

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// JavaScript Code is here ...
+
+
+var car = {
+    make: 'VW',
+    type: 'Polo',
+    color: 'blue',
+    isTurnedOn: false,
+    numberOfWheels: 4,
+    seats: [
+        'seat1',
+        'seat2',
+        'seat3',
+        'seat4'
+    ],
+    switchCar: function(isOn) {
+        if (isOn == true) {
+            this.isTurnedOn = true;
+            console.log("turn car ON");
+        } else {
+            this.isTurnedOn = false;
+            console.log("car is Off");
+        }
+    },
+};
